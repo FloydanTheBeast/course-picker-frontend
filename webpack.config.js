cdee8839a2d3824d7e56e62386883ca67ab5e36e
Abdelsalam Shadi <floydanthebeast@gmail.com>
2021-02-10 01:46:50 +0300

    build(configs): Настроены основные конфиги
    Написаны базовые конфиги для webpack, typescript, eslint, prettier
    Добавлен код для проверки работоспособности

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+	entry: './src/index.tsx',
+	mode: 'development',
+	devtool: 'eval',
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: 'bundle.js',
+	},
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js'],
+	},
+	devServer: {
+		contentBase: './build',
+		historyApiFallback: true,
+		port: 8080,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts(x?)$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'ts-loader',
+					},
+				],
+			},
+			{
+				enforce: 'pre',
+				test: /\.js$/,
+				loader: 'source-map-loader',
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+		}),
+	],
+}
