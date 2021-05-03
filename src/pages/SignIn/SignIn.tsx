88adfbf75888672cd37b8005dd3cba5aeb689760
Abdelsalam Shadi <floydanthebeast@gmail.com>
2021-05-04 00:56:22 +0300

    feat: Валидация при регистрации и входе - Компонент MessageBox для отображения сообщений - Валидация всех полей

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,24 +1,32 @@
 import Button from "components/Button";
 import Input from "components/Input";
+import MessageBox from "components/MessageBox";
 import { useAuth } from "providers/authProvider";
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Redirect } from "react-router-dom";
 import { SignInData } from "services/authService";
 import { Label, SignInForm } from "./styled";
 
-const SignIn = () => {
-	const { register, handleSubmit } = useForm();
+const SignIn: React.FC = () => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors }
+	} = useForm();
+
 	const {
 		authState: { isAuthenticated },
 		dispatchAuthState,
 		signin
 	} = useAuth();
 
+	const [serverError, setServerError] = useState("");
+
 	const onSubmit = (data: SignInData & { emailOrUsername: string }) => {
 		let signInData: SignInData;
 
-		if (/\S+@\S+\.\S+/.test(data.emailOrUsername)) {
+		if (/^\S+@\S+\.\S+$/.test(data.emailOrUsername)) {
 			signInData = {
 				email: data.emailOrUsername,
 				password: data.password
@@ -36,26 +44,50 @@ const SignIn = () => {
 					type: "signin",
 					payload: res.data
 				});
-				// setIsAuthenticated(true);
 			})
-			// TODO: Обрабатывать ошибку соответствующе, показывать
-			.catch((error) => console.log(error.response));
+			.catch((error) =>
+				setServerError(
+					error.response?.data?.error || "Внутренная ошибка сервера"
+				)
+			);
 	};
 
 	return isAuthenticated ? (
 		<Redirect to="/" />
 	) : (
-		// TODO: Добавить валидацию
 		<SignInForm onSubmit={handleSubmit(onSubmit)}>
+			{serverError && (
+				<MessageBox
+					type="error"
+					message={serverError}
+					primary
+					closable
+					onClose={() => setServerError("")}
+					closeTimeout={3000}
+				/>
+			)}
 			<Label htmlFor="">Электронная почта/имя пользователя</Label>
+			{errors.emailOrUsername && (
+				<MessageBox
+					type="error"
+					message={errors.emailOrUsername.message}
+				/>
+			)}
 			<Input
-				{...register("emailOrUsername")}
+				{...register("emailOrUsername", {
+					required: { value: true, message: "Обязательное поле" }
+				})}
 				placeholder="Введите адрес электронной почты"
 			/>
 			<Label htmlFor="">Пароль</Label>
+			{errors.password && (
+				<MessageBox type="error" message={errors.password.message} />
+			)}
 			<Input
 				type="password"
-				{...register("password")}
+				{...register("password", {
+					required: { value: true, message: "Обязательное поле" }
+				})}
 				placeholder="Введите пароль"
 			/>
 			<Button type="submit">Войти</Button>
--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,5 +1,6 @@
 import Button from "components/Button";
 import Input from "components/Input";
+import MessageBox from "components/MessageBox";
 import { useAuth } from "providers/authProvider";
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
@@ -8,12 +9,18 @@ import { SignUpData } from "services/authService";
 import { Label, SignUpForm } from "./styled";
 
 const SignUp = () => {
-	const { register, handleSubmit } = useForm();
+	const {
+		register,
+		handleSubmit,
+		formState: { errors }
+	} = useForm();
 	const [hasSignedUp, setHasSignedUp] = useState(false);
 	const {
 		authState: { isAuthenticated },
 		signup
 	} = useAuth();
+
+	const [serverError, setServerError] = useState("");
 
 	const onSubmit = (data: SignUpData) => {
 		signup(data)
@@ -21,29 +28,61 @@ const SignUp = () => {
 				setHasSignedUp(true);
 			})
 			// TODO: Обрабатывать ошибку соответствующе, показывать
-			.catch((error) => console.log(error.response));
+			.catch((error) =>
+				setServerError(
+					error.response?.data?.error || "Внутренная ошибка сервера"
+				)
+			);
 	};
 
 	// TODO: Сообщение об успехе регистрации
 	return isAuthenticated || hasSignedUp ? (
 		<Redirect to="/" />
 	) : (
-		// TODO: Добавить валидацию
 		<SignUpForm onSubmit={handleSubmit(onSubmit)}>
+			{serverError && (
+				<MessageBox
+					type="error"
+					message={serverError}
+					primary
+					closable
+					onClose={() => setServerError("")}
+					closeTimeout={3000}
+				/>
+			)}
 			<Label htmlFor="">Электронная почта</Label>
+			{errors.email && (
+				<MessageBox type="error" message={errors.email.message} />
+			)}
 			<Input
-				{...register("email")}
+				{...register("email", {
+					pattern: {
+						value: /^\S+@\S+\.\S+$/,
+						message: "Некорректный адрес"
+					},
+					required: { value: true, message: "Обязательное поле" }
+				})}
 				placeholder="Введите адрес электронной почты"
 			/>
 			<Label htmlFor="">Имя пользователя</Label>
+			{errors.username && (
+				<MessageBox type="error" message={errors.username.message} />
+			)}
 			<Input
-				{...register("username")}
+				{...register("username", {
+					required: { value: true, message: "Обязательное поле" }
+				})}
 				placeholder="Введите имя пользователя"
 			/>
 			<Label htmlFor="">Пароль</Label>
+			{errors.password && (
+				<MessageBox type="error" message={errors.password.message} />
+			)}
 			<Input
 				type="password"
-				{...register("password")}
+				{...register("password", {
+					required: { value: true, message: "Обязательное поле" }
+				})}
 				placeholder="Введите пароль"
 			/>
 			<Button type="submit">Зарегистрироваться</Button>
